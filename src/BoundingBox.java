8d5d6400ffb2fd29c503576869553de9e8360151
tom <florentinb240@hotmail.fr>
2019-09-02 23:52:06 +0200

    Restructed the project (issues with packages)

--- src/BoundingBox.java
+++ src/BoundingBox.java
@@ -1,3 +1,4 @@
+
 public class BoundingBox{
 
     int sizeX;
--- src/Type.java
+++ src/Type.java
@@ -1,3 +1,4 @@
+
 public class Type{
 
     int DefautlSizeClass;
@@ -15,5 +16,17 @@ public class Type{
     public Type(int default_size_class)
     {
         this.DefautlSizeClass = default_size_class;
+    }
+
+
+    public BoundingBox getBB(Type t)
+    {
+        //TODO: e tailles seront à changer, ici elles sont à titre d'exemple
+        if (t.DefautlSizeClass ==1 )
+        {
+            return new BoundingBox(3, 5);
+        }
+
+        return null;
     }
 }
