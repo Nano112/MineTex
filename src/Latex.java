272384bd591a83250a8efd7782640ecf4098e1c4
Omeganx <florentinb240@hotmail.fr>
2019-09-04 13:38:16 +0200

    Update Latex.java

--- src/Latex.java
+++ src/Latex.java
@@ -5,15 +5,15 @@ public class Latex //C'est pas du Latex Faudras changer de nom je pense
      /*
      HOWTO:
 
-     Chaque élément de base à une taille (Bounding Box) par défault.
+     Chaque élément de base a une taille (Bounding Box) par défault.
      Lorsque que l'on parse le code Latex, on aura un arbre avec des lettres et des chiffres en tant que feuilles
      et des symboles/opérand en tant que noeuds.
-     Chaque Operan et symbole a des conditions sur la taille (pour \sum_{k=1}^{n}{f(k)} il faut que la taille de k=1 et de n soit inférieure à f(k) -> D'ou le sizeClass.
+     Chaque Operand et symbole a des conditions sur la taille (pour \sum_{k=1}^{n}{f(k)} il faut que la taille de k=1 et de n soit inférieure à f(k) -> D'ou le sizeClass.
      (sizeClass = hauteur du caratère? )
      -On remonte l'arbre, en adjustant les bounding box pour que l'expression soit 'esthétique':
         exemple: 1+\sqrt{\frac{3}{5}}
           --> la sizeClass de \sqrt{\frac{3}{5}} sera plus grande que la sizeClass de sqrt{\frac{3}{5}} -> On augment la size class de 1 pour qu'elle y soit égale.
-           (Il faut aussi fiare attention pour la largeur en (x) ?) (Exemple \sum_{k=f(1+\sqrt{4})}{k} les caractères s'intersecteraient)
+           (Il faut aussi faire attention pour la largeur en (x) ?) (Exemple \sum_{k=f(1+\sqrt{4})}{k} les caractères s'intersecteraient)
 
 
 
@@ -26,6 +26,14 @@ public class Latex //C'est pas du Latex Faudras changer de nom je pense
      Osef du calcul tensoriel? (vecteur, le \bar?, produit contracté, matrices)
      ^
      Pendant qu'on y est oui mais deja faut avoir les operations de base plus des trucs moin basique comme des sommes
-      */
+      
+     Parser
+     Toutes les tiles 
+     TileReader
+     Render
+     SizeManager 
+     ExpressionTree
+
+*/
 
 }
