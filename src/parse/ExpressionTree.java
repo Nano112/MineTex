aa8d6d51289547e003afa465929629597dd3336c
tom <florentinb240@hotmail.fr>
2019-09-30 20:11:47 +0200

    Optimized and fixed the parser.

--- src/parse/ExpressionTree.java
+++ src/parse/ExpressionTree.java
@@ -16,9 +16,9 @@ public class ExpressionTree {
     String[] currentExpression;
     boolean root = false;
     boolean hasMathObject;
-
     MathObject mObject;
     int offset;
+
     public ExpressionTree(String curent_)
     {
         this.currentExpression = curent_.split("");
@@ -76,18 +76,15 @@ public class ExpressionTree {
         while(i < this.currentExpression.length) {
             if (this.currentExpression[i].equals("{")) //We do not take into account the binary operands inside brackets -> they will be handled afterwards.
             {
-
                 i = findNextBracket(i)-1;
             }
             if (Tiles.isBinaryOperand(this.currentExpression[i])) {
                 this.mObject = new MathObject(this.currentExpression[i], TileType.BOperand);
                 this.hasMathObject = true;
-
                 if (i == 0 || i + 1 == this.currentExpression.length) {
                     throw new BadFormatting();
                 } else {
                     binaryOperandIndex = i;
-
                     childsLeft.add(new ExpressionTree(subString(this.currentExpression, 0, binaryOperandIndex), this.offset));
                     childsRight.add(new ExpressionTree(subString(this.currentExpression, binaryOperandIndex + 1, this.currentExpression.length), this.offset + binaryOperandIndex));
                     childsLeft.get(0).parse();
@@ -98,17 +95,13 @@ public class ExpressionTree {
             i++;
         }
         i = 0;
-
-
         while(i < this.currentExpression.length)//Step 3
         {
             if(this.currentExpression[i].equals("\\"))
             {
-
                 String biggreekletter = Tiles.lookForBigGreekLetter(this.currentExpression, i);
                 String smallgreekletter = Tiles.lookForBigGreekLetter(this.currentExpression, i);
                 String tileoperandletter = Tiles.loofFortilesOperands(this.currentExpression,i);
-
                 if(biggreekletter != null)
                 {
                     MathObject mob = new MathObject(biggreekletter, TileType.BigGreekLetter);
@@ -122,21 +115,22 @@ public class ExpressionTree {
                 //operand case
                 else if(tileoperandletter != null) //uh oh! We need to look for the brackets after the operand!
                 {
-
                     int bracketsneeded = TileOperand.getBracketsNeeded(tileoperandletter); //the number of brackets we need
                     int index = i + tileoperandletter.length()-1; //We are going to browse the rest of the expression in order to find the brackets
-                    int ind = i; //for the Error message
+                    int ind = i;
                     MathObject mob = new MathObject(tileoperandletter, TileType.Operand);
                     ArrayList<ExpressionTree> bracketsExpression = new ArrayList<>();
                     while(index < this.currentExpression.length)
-                    {
-
-                        //cas ou l'on a "^{machin..}" ou "_{machin...}"
+                    {   //cas ou l'on a "^{machin..}" ou "_{machin...}"
                         if(index < this.currentExpression.length-2 && (this.currentExpression[index].equals("_") || this.currentExpression[index].equals("^")) && this.currentExpression[index+1].equals("{"))
                         {
                             int nextbracketindex = findNextBracket(index+1)-1;
                             MathObject mobj = new MathObject(this.currentExpression[i]+"{}", TileType.Brackets);
-                            bracketsExpression.add(new ExpressionTree(subString(this.currentExpression, index+1, nextbracketindex), this.offset+index, mobj));
+                            ExpressionTree exp = new ExpressionTree(mobj, this.offset+index);
+                            ExpressionTree newchild = new ExpressionTree(subString(this.currentExpression, index+1, nextbracketindex), this.offset);
+                            newchild.parse();
+                            exp.childsRight.add(newchild);
+                            bracketsExpression.add(exp);
                             bracketsneeded--;
                             index = nextbracketindex;
                         }
@@ -144,53 +138,51 @@ public class ExpressionTree {
                         if(this.currentExpression[index].equals("{"))
                         {
                             int nextbracketindex = findNextBracket(index)-1;
-
                             MathObject mobj = new MathObject("{}", TileType.Brackets);
                             ExpressionTree exp = new ExpressionTree(mobj, this.offset+index);
-                            exp.childsRight.add(new ExpressionTree(subString(this.currentExpression, index+1, nextbracketindex), this.offset));
+                            ExpressionTree newchild = new ExpressionTree(subString(this.currentExpression, index+1, nextbracketindex), this.offset);
+                            newchild.parse();
+                            exp.childsRight.add(newchild);
                             bracketsExpression.add(exp);
                             bracketsneeded--;
                             index = nextbracketindex;
                         }
                         if(bracketsneeded == 0)
-                        {    //If we have nothing left, we can just put the oject inside the treeExpression, otherwise we need to create a branch we a concat operator
+                        {   //If we have nothing left, we can just put the oject inside the treeExpression, otherwise we need to create a branch we a concat operator
                             ExpressionTree opexp = new ExpressionTree(mob, this.offset+i);
+                            this.hasMathObject = true;
                             opexp.childsRight = bracketsExpression;
-                            if(index == this.currentExpression.length-1)
+                            String[] reste_gauche = subString(this.currentExpression, 0, ind);
+                            String[] reste_droite = subString(this.currentExpression, index+1, this.currentExpression.length);
+                            if(reste_droite.length == 0 && reste_gauche.length == 0)
                             {
-                                if(!this.hasMathObject)
-                                {
-                                    this.mObject = mob;
-                                    this.hasMathObject = true;
-                                    this.childsRight= bracketsExpression;
-                                }else{
+                                this.mObject = mob;
+                                this.childsRight= bracketsExpression;
+                            }else if(reste_gauche.length == 0 && reste_droite.length != 0){
 
-                                    this.childsRight.add(opexp);
-                                }
-                            }else{
-                                String[] reste = subString(this.currentExpression, index+1, this.currentExpression.length);
-                                ExpressionTree resteexp = new ExpressionTree(reste, this.offset+index+1);
-                                MathObject concat = MathObject.concat;
-                                ExpressionTree et = new ExpressionTree(concat, this.offset+index);
-                                et.childsRight.add(opexp);
-                                et.childsLeft.add(resteexp);
-                                if(this.hasMathObject)
-                                {
-                                    this.childsLeft.add(et);
-                                }else{
-                                    this.mObject = concat;
-                                    this.hasMathObject = true;
-                                    this.childsLeft.add(opexp);
-                                    this.childsRight.add(resteexp);
-                                }
-                            }
-                            for(ExpressionTree et : this.childsRight)
-                            {
-                                et.parse();
-                            }
-                            for(ExpressionTree et : this.childsLeft)
-                            {
-                                et.parse();
+                                ExpressionTree resteexp = new ExpressionTree(reste_droite, this.offset+index+1);
+                                resteexp.parse();
+                                this.mObject = MathObject.concat;
+                                this.childsLeft.add(opexp);
+                                this.childsRight.add(resteexp);
+                            }else if(reste_gauche.length != 0 && reste_droite.length == 0){
+                                ExpressionTree resteexp = new ExpressionTree(reste_gauche, this.offset+index+1);
+                                resteexp.parse();
+                                this.mObject = MathObject.concat;
+                                this.childsRight.add(opexp);
+                                this.childsLeft.add(resteexp);
+                            }else if(reste_gauche.length != 0 && reste_droite.length != 0) {
+                                ExpressionTree dr = new ExpressionTree(reste_droite, this.offset + index + 1);
+                                ExpressionTree le = new ExpressionTree(reste_gauche, this.offset + index + 1);
+                                le.parse();
+                                dr.parse();
+                                this.mObject = MathObject.concat;
+                                ExpressionTree obleft = new ExpressionTree(MathObject.concat, this.offset + index + 1);
+                                obleft.childsLeft.add(le);
+                                obleft.childsRight.add(opexp);
+                                this.childsRight.add(dr);
+                                this.childsLeft.add(obleft);
+
                             }
                             i = index;
                             return;
@@ -239,7 +231,7 @@ public class ExpressionTree {
         if(this.hasMathObject)
         {
 
-            System.out.println(line+"    id=\""+this.mObject.expression+"\"");
+            System.out.println(line+"    id=\""+this.mObject.expression+"\""+ ",type="+this.mObject.tt);
         }else{
             String res = "";
             for(int i = 0; i < this.currentExpression.length; i++)
